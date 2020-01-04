fb67370c36c68b72663301059ed250595da9ecd1
jchrys <jchrys@me.com>
2020-01-04 23:55:40 +0900

    :tada: now it's working anyway todo->bulkdump, multiple search keywords

--- NBCrawler/core/utils.py
+++ NBCrawler/core/utils.py
@@ -12,6 +12,9 @@ from bs4 import BeautifulSoup
 
 re_all = re.compile('<[^>]*>')
 def drop_html_tag_all(html):
+    if html == None:
+        return None
+    #print(type(html))
     return re.sub(re_all, '', html)
 
 def get_drop_html_tag_but(tag):
@@ -29,7 +32,7 @@ def get_html_content_by_id(url, ids=[]) -> dict:
         raise Exception("not a list")
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
-    return { id: soup.find(id=id) for id in ids }
+    return { id: soup.find(id=id).text if soup.find(id=id) else None for id in ids }
 
 if __name__ == '__main__':
     print(drop_html_tag_all("<strong> Hello World!</strong>"))
--- NBCrawler/main.py
+++ NBCrawler/main.py
@@ -1,8 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : main.py
+# Author            : JCHRYS <jchrys@me.com>
+# Date              : 04.01.2020
+# Last Modified Date: 04.01.2020
+# Last Modified By  : JCHRYS <jchrys@me.com>
 import settings
 import platform
+import time
+import json
+import os
+from NBCrawler.core import utils
+from NBCrawler.core.requester import Request
+
+from pprint import pprint
 
 ## os_valdation ##
 if (platform.system() != 'Darwin'):
     print('your OS is not supported')
     exit()
+##TODO header, params, url 
+print("=======give me your keyword")
+request = Request(settings.default_headers, settings.default_params, settings.default_url)
+keyword = input()
+res = request.search(keyword).json()
+detail_ids = ['authorIntroContent', 'bookIntroContent', 'tableOfContentsContent']
+items = res['items']
+print(len(items))
 
+for item in items:
+    detail_dict = utils.get_html_content_by_id(item['link'], detail_ids)
+    item.update(detail_dict)
+    for key in item.keys():
+        item[key] = utils.drop_html_tag_all(item[key])
+    
+
+with open(os.path.join(settings.path_to_output, 'filename.json'), 'w', encoding='utf8') as file:
+    json.dump(items, file, ensure_ascii=False, indent=4)
