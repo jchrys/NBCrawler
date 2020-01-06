83ee7cf421358e94dbfc6d65c654e5f6ec79c4da
jchrys <jchrys@me.com>
2020-01-06 13:59:22 +0900

    dist

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : setup.py
+# Author            : JCHRYS <jchrys@me.com>
+# Date              : 06.01.2020
+# Last Modified Date: 06.01.2020
+# Last Modified By  : JCHRYS <jchrys@me.com>
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
         name="NBCrawler", 
-        version="1.0", 
+        version="0.0.1", 
         packges= find_packages(),
         description="Book information crawler using 'NAVER BOOKS API'",
         author="JCHRYS",
         author_email="jchrys@me.com",
-        install_requires=["requests",""],
-        scripts = [
-            ] 
+        long_description=long_description,
+        url='https://github.com/jchrys/NBCrawler',
+        install_requires=["requests",
+            "bs4", 
+            "tqdm",
+            ],
+        python_requires='>=3.6',
+        classfiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved:: MIT License",
+            ]
         )
