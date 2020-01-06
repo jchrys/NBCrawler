adc16ea9eeb06857d9fc6a03cf891c86b4fd8f8a
jchrys <jchrys@me.com>
2020-01-06 10:20:38 +0900

    :tada: progress bar, isbn split

--- NBCrawler/main.py
+++ NBCrawler/main.py
@@ -10,30 +10,34 @@ import platform
 import time
 import json
 import os
+from tqdm import tqdm
 from NBCrawler.core import utils
 from NBCrawler.core.requester import Request
 
 from pprint import pprint
 
-## os_valdation ##
+## os_validation ##
 if (platform.system() != 'Darwin'):
-    print('your OS is not supported')
+    print('Your OS is not supported')
     exit()
 ##TODO header, params, url 
-print("=======give me your keyword")
 request = Request(settings.default_headers, settings.default_params, settings.default_url)
-keyword = input()
+keyword = input('search_by_keyword: ')
+output_filename = input('output_filename: ')
 res = request.search(keyword).json()
 detail_ids = ['authorIntroContent', 'bookIntroContent', 'tableOfContentsContent']
 items = res['items']
-print(len(items))
 
-for item in items:
+for item in tqdm(items):
     detail_dict = utils.get_html_content_by_id(item['link'], detail_ids)
     item.update(detail_dict)
     for key in item.keys():
         item[key] = utils.drop_html_tag_all(item[key])
-    
+    if 'isbn' in item:
+        isbn10, isbn13 = item['isbn'].split()
+        item['isbn10'] = isbn10
+        item['isbn13'] = isbn13
 
-with open(os.path.join(settings.path_to_output, 'filename.json'), 'w', encoding='utf8') as file:
+
+with open(os.path.join(settings.path_to_output, output_filename + '.json'), 'w', encoding='utf8') as file:
     json.dump(items, file, ensure_ascii=False, indent=4)
